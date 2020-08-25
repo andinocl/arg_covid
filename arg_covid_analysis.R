961e46e99436525a3c677a0f0cb0e8206370ea90
andinocl <andinocl@gmail.com>
2020-08-25 10:00:26 -0300

    Start of a shiny

---
+++ arg_covid_analysis.R
@@ -0,0 +1,16 @@
+#
+#
+
+# load RData
+load("CovidEpiFile.RData")
+
+# Run reports on CABA, San Isidro, and immediately surrounding
+
+# Find R for each province
+
+# Run reports on 5x worst provinces
+
+# Find R for each PBA municipality
+
+# Run reports on 5x worst, 3x best
+
